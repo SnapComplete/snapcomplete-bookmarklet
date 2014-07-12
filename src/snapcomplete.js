e6aa54f19d01664d411897710c8945694e7180e5
Hoang Nguyen <hoang.nguyen.11084@gmail.com>
2014-07-11 17:50:17 -0700

    Added IIFE

--- src/snapcomplete.js
+++ src/snapcomplete.js
@@ -1,12 +1,14 @@
-var baseURL = 'https://www.snapcomplete.com';
-var s = document.createElement('script');
-s.onreadystatechange = s.onload = function () {
-  var state = s.readyState;
-  var callback = SC.init;
-  if (!callback.done && (!state || /loaded|complete/.test(state))) {
-    callback.done = true;
-    callback({'showImmediately': true});
-  }
-};
-s.src = baseURL + '/js/plugin/embed.js';
-document.body.appendChild(s);
+!function() {
+  var baseURL = 'https://www.snapcomplete.com';
+  var s = document.createElement('script');
+  s.onreadystatechange = s.onload = function () {
+    var state = s.readyState;
+    var callback = SC.init;
+    if (!callback.done && (!state || /loaded|complete/.test(state))) {
+      callback.done = true;
+      callback({'showImmediately': true});
+    }
+  };
+  s.src = baseURL + '/js/plugin/embed.js';
+  document.body.appendChild(s);
+}();
